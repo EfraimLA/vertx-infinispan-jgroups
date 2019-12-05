5a5342b96b8bdd188f3d4a71736185b664772b56
Efraim LA <efraimlopezamador123@gmail.com>
2019-12-05 11:38:50 -0600

    Optimized script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,6 +1,7 @@
 oc new-project eventbus
-oc policy add-role-to-user admin efraimla -n eventbus
-oc policy add-role-to-user view -n eventbus -z default
+oc project eventbus
+oc policy add-role-to-user admin efraimla -n $(oc project -q)
+oc policy add-role-to-user view -n $(oc project -q) -z default
 oc policy add-role-to-user view system:serviceaccount:$(oc project -q):default -n $(oc project -q)
 
 ./mvnw clean fabric8:deploy

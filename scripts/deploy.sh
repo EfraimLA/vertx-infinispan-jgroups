306f6e52fd958de612c106f5ccca3950e6927099
Efraim LA <efraimlopezamador123@gmail.com>
2019-12-06 11:49:31 -0600

    Optimized configmap creation //tofix existent error

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,5 +3,6 @@ oc project eventbus
 oc policy add-role-to-user admin efraimla -n $(oc project -q)
 oc policy add-role-to-user view -n $(oc project -q) -z default
 oc policy add-role-to-user view system:serviceaccount:$(oc project -q):default -n $(oc project -q)
+echo $CONFIG_MAP >> configmap.yml && oc create configmap config --from-file=configmap.yml
 
 #mvn clean fabric8:deploy

681c0b825c5040cfc65ba46c04cd5b968611f2dc
Efraim LA <efraimlopezamador123@gmail.com>
2019-12-06 00:53:41 -0600

    Removed Maven Wrapper Fixed maven action error (curl) Use mvnw to mvn

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,4 +4,4 @@ oc policy add-role-to-user admin efraimla -n $(oc project -q)
 oc policy add-role-to-user view -n $(oc project -q) -z default
 oc policy add-role-to-user view system:serviceaccount:$(oc project -q):default -n $(oc project -q)
 
-./mvnw clean fabric8:deploy
+mvn clean fabric8:deploy

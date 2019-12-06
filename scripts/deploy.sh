4264af4f442df8ecce47e9c784cc0999377888c0
Efraim LA <efraimlopezamador123@gmail.com>
2019-12-06 11:53:54 -0600

    Fixes

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,8 +1,11 @@
 oc new-project eventbus
 oc project eventbus
+
 oc policy add-role-to-user admin efraimla -n $(oc project -q)
 oc policy add-role-to-user view -n $(oc project -q) -z default
 oc policy add-role-to-user view system:serviceaccount:$(oc project -q):default -n $(oc project -q)
+
+oc delete configmap config
 echo $CONFIG_MAP >> configmap.yml && oc create configmap config --from-file=configmap.yml
 
 #mvn clean fabric8:deploy

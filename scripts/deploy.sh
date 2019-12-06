40522db45cff595f697dea38d8de32b2f6987574
Efraim LA <efraimlopezamador123@gmail.com>
2019-12-06 11:57:47 -0600

    Optimization

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,7 +5,6 @@ oc policy add-role-to-user admin efraimla -n $(oc project -q)
 oc policy add-role-to-user view -n $(oc project -q) -z default
 oc policy add-role-to-user view system:serviceaccount:$(oc project -q):default -n $(oc project -q)
 
-oc delete configmap config
-echo $CONFIG_MAP >> configmap.yml && oc create configmap config --from-file=configmap.yml
+echo $CONFIG_MAP >> configmap.yml && oc apply -f configmap.yml
 
 #mvn clean fabric8:deploy

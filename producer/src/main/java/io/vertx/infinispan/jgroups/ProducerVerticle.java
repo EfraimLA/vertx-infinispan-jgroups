3c39cea4292962888380c4f3a529bbe510166391
Efraim LA <efraimlopezamador123@gmail.com>
2020-01-05 19:29:03 -0600

    Updated

--- producer/src/main/java/io/vertx/infinispan/jgroups/ProducerVerticle.java
+++ producer/src/main/java/io/vertx/infinispan/jgroups/ProducerVerticle.java
@@ -1,12 +1,9 @@
 package io.vertx.infinispan.jgroups;
 
-import io.vertx.core.http.HttpServerOptions;
 import io.vertx.core.logging.Logger;
 import io.vertx.core.logging.LoggerFactory;
-import io.vertx.core.net.PfxOptions;
 import io.vertx.reactivex.core.AbstractVerticle;
 import io.vertx.reactivex.core.eventbus.EventBus;
-import io.vertx.reactivex.ext.web.Router;
 
 import java.util.concurrent.ThreadLocalRandom;
 
@@ -26,31 +23,18 @@ public class ProducerVerticle extends AbstractVerticle {
             eb.send("stats", stats);
         });
 
-        final Router router = Router.router(vertx);
+        vertx.createHttpServer()
+                .requestHandler(rc -> {
+                    LOGGER.info("Handling request & sending message...");
 
-        router.get("/").handler(rc -> {
-            LOGGER.info("Handling request & sending message...");
+                    String message = "This is the message: Hello cx";
 
-            String message = "This is the message: Hello cx";
+                    if (rc.getParam("message") != null) message = rc.getParam("message");
 
-            if (rc.request().getParam("message") != null) message = rc.request().getParam("message");
+                    eb.send("message", message);
 
-            eb.send("message", message);
-
-            rc.response().end("Message Sent!");
-        });
-
-        final HttpServerOptions serverOptions = new HttpServerOptions()
-                .setSsl(true)
-                .setPfxKeyCertOptions(
-                        new PfxOptions()
-                        .setPath("selfsigned.jks")
-                        .setPassword(System.getenv("CERT_PASSWORD"))
-                );
-
-        vertx.createHttpServer(serverOptions)
-                .requestHandler(router)
-                .rxListen(config().getInteger("http.port", 8080))
+                    rc.response().end("Message Sent!");
+                }).rxListen(config().getInteger("http.port", 8080))
                 .subscribe(httpServer -> LOGGER.info("Server started successfully!"));
     }
 }
